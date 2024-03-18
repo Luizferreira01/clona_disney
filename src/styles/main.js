e2882d42bb3dc6ee5b42ad0d5c16704a2967fb8c
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-03-18 20:45:14 -0300

    Update index.html

--- src/styles/main.js
+++ src/styles/main.js
@@ -3,6 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = this.window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    // seçao de atraçoes, programaçao das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -18,6 +32,16 @@ document.addEventListener('DOMContentLoaded', function() {
         questions[i].addEventListener('click', AbreOuFechaResposta);
     }
 })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function AbreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
